eeef863482eb3acec71d978a97949293e5cfa697
kanatani-y <yoshifumi.kanatani@mail.o-hara.ac.jp>
2022-08-26 17:15:32 +0900

    メール送信の実装、設定ファイルを環境別に分割

--- diary/views.py
+++ diary/views.py
@@ -1,11 +1,25 @@
+import logging
+
+from django.contrib import messages
+from django.urls import reverse_lazy
 from django.views import generic
 
 from .forms import InquiryForm
 
+logger = logging.getLogger(__name__)
+
+# トップページ
 class IndexView(generic.TemplateView):
-  template_name = "index.html"
+    template_name = "index.html"
 
-
+# お問合せページ
 class InquiryView(generic.FormView):
-  template_name = "inquiry.html"
-  form_class = InquiryForm
+    template_name = "inquiry.html"
+    form_class = InquiryForm
+    success_url = reverse_lazy('diary:inquiry')
+
+    def form_valid(self, form):
+        form.send_email()
+        messages.success(self.request, 'メッセージを送信しました。')
+        logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
+        return super().form_valid(form)
---
+++ private_diary/settings_dev.py
@@ -0,0 +1,51 @@
+from .settings_common import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-nuf303@42i(rg@ma$*dqi_e0nk1dz7)6$by4*3boe=vk_*_xx^'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# ロギング設定
+LOGGING = {
+    'version': 1,  # 1固定
+    'disable_existing_loggers': False,
+
+    # ロガーの設定
+    'loggers': {
+        # Djangoが利用するロガー
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+        # diaryアプリケーションが利用するロガー
+        'diary': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+    # ハンドラの設定
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'dev'
+        },
+    },
+
+    # フォーマッタの設定
+    'formatters': {
+        'dev': {
+            'format': '\t'.join([
+                '%(asctime)s',
+                '[%(levelname)s]',
+                '%(pathname)s(Line:%(lineno)d)',
+                '%(message)s'
+            ])
+        },
+    }
+}
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
